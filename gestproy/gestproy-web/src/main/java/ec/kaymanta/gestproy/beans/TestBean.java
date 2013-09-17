cfe524adc0e3f111a3340b4e440169bfb04837df
SchubertD <schubert_david@Schubert-David-Rodriguezs-MacBook-Pro.local>
2013-09-17 15:42:01 -0500

    gestproy, funcionando
    Levanta la pantalla de index en JSP, referencias ejb, completas

---
+++ gestproy/gestproy-web/src/main/java/ec/kaymanta/gestproy/beans/TestBean.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.kaymanta.gestproy.beans;
+
+import ec.kaymanta.gestproy.servicio.UsuarioServicio;
+import javax.ejb.EJB;
+
+/**
+ *
+ * @author schubert_david
+ */
+public class TestBean {
+    @EJB
+    UsuarioServicio usuarioServicio;
+}
